b65fbcf3363cb44105f7a322fc3a8b6f3a8f4300
LucasScheffer18 <101944231+LucasScheffer18@users.noreply.github.com>
2024-04-10 18:21:24 -0300

    Verificar

---
+++ CLINICA/Cadastro_Paciente.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de Pacientes</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <h2>Cadastro de Pacientes</h2>
+        <!-- Campo paciente_id oculto -->
+        <input type="hidden" id="paciente_id" name="paciente_id">
+
+        <label for="nome">Nome:</label>
+        <input type="text" id="nome" name="nome" required>
+
+        <label for="data_nascimento">Data de Nascimento:</label>
+        <input type="date" id="data_nascimento" name="data_nascimento" required>
+
+        <label for="endereco">Endereço:</label>
+        <input type="text" id="endereco" name="endereco" required>
+
+        <label for="telefone">Telefone:</label>
+        <input type="tel" id="telefone" name="telefone" pattern="\([0-9]{2}\) [0-9]{5}-[0-9]{4}" placeholder="(XX) XXXXX-XXXX" required>
+
+        <input type="submit" value="Cadastrar">
+    </form>
+
+    <?php
+        $conn = new mysqli("localhost", "root", "", "clinica");
+    ?>
+    <script>
+        // Adicionando máscara ao campo de telefone
+        window.onload = function() {
+            var telefoneInput = document.getElementById('telefone');
+            telefoneInput.addEventListener('input', function() {
+                var telefone = telefoneInput.value;
+                telefone = telefone.replace(/\D/g, ''); // Remove caracteres não numéricos
+                var formattedTelefone = '(' + telefone.substring(0, 2) + ') ' + telefone.substring(2, 7) + '-' + telefone.substring(7, 11);
+                telefoneInput.value = formattedTelefone;
+            });
+        };
+    </script>
+</body>
+</html>
